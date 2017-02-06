c481a692a06e2af128f991b936bd75ce25fd5b3e
a.semenkov <sealexey@gmail.com>
2017-02-06 19:02:11 +0300

    + fix template path + up damsel

--- src/main/java/com/rbkmoney/walker/config/FreeMarkerConfig.java
+++ src/main/java/com/rbkmoney/walker/config/FreeMarkerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
@@ -15,15 +16,12 @@ import java.io.IOException;
 @Configuration
 public class FreeMarkerConfig {
 
-    @Value("templates/")
-    private ClassPathResource cpr;
-
     private freemarker.template.Configuration cfg;
 
     @Bean
     public DescriptionBuilder descriptionBuilder() throws IOException {
         cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_25);
-        cfg.setDirectoryForTemplateLoading(cpr.getFile());
+        cfg.setClassForTemplateLoading(this.getClass(), "/templates/");
         cfg.setDefaultEncoding("UTF-8");
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
         cfg.setLogTemplateExceptions(false);
