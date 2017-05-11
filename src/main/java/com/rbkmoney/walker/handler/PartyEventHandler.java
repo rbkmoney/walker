2149fdeb112ffbeb36981f0032960028361a6e24
Alexey Semenkov <sealexey@gmail.com>
2017-05-11 17:59:49 +0300

    add ignore partyId (#21)
    ATTANTION CHERKASHI
    * add ignore partyId
    * add todo and dumb test
    
    WALK-31

--- src/main/java/com/rbkmoney/walker/handler/PartyEventHandler.java
+++ src/main/java/com/rbkmoney/walker/handler/PartyEventHandler.java
@@ -45,6 +45,15 @@ public class PartyEventHandler implements Handler<StockEvent> {
         if (!event.getPayload().isSetPartyEvent()) {
             return;
         }
+
+        String partyId = event.getSource().getParty();
+        //TODO: HARDCODE to be removed
+        if (partyId.equals("39d17eca-0239-4ed8-8e32-dc78cf589135")) {
+            log.error("Claim for party withId 39d17eca-0239-4ed8-8e32-dc78cf589135 will be ignored, eventId: {}", event.getId());
+            return;
+        }
+        // TODO: to be removed
+
         if (event.getPayload().getPartyEvent().isSetClaimCreated()) {
             log.info("Got ClaimCreated event with EventID: {}", eventId);
             if (event.getPayload().getPartyEvent().getClaimCreated().getStatus().isSetAccepted()) {
@@ -53,7 +62,7 @@ public class PartyEventHandler implements Handler<StockEvent> {
                 createIssue(event);
             }
         } else if (event.getPayload().getPartyEvent().isSetClaimStatusChanged()) {
-            String partyId = event.getSource().getParty();
+
             log.info("Got ClaimStatusChanged event with EventID: {}", eventId);
             ClaimStatusChanged claimStatusChanged = event.getPayload().getPartyEvent().getClaimStatusChanged();
             if (claimStatusChanged.getStatus().isSetRevoked()) {
--- src/test/java/com/rbkmoney/JiraDaoTest.java
+++ src/test/java/com/rbkmoney/JiraDaoTest.java
@@ -1,7 +1,11 @@
 package com.rbkmoney;
 
 import com.rbkmoney.config.TestJiraConfig;
+import com.rbkmoney.damsel.event_stock.SourceEvent;
+import com.rbkmoney.damsel.event_stock.StockEvent;
+import com.rbkmoney.damsel.payment_processing.*;
 import com.rbkmoney.walker.dao.JiraDao;
+import com.rbkmoney.walker.handler.PartyEventHandler;
 import net.rcarz.jiraclient.*;
 import net.sf.json.JSON;
 import org.junit.Ignore;
@@ -18,7 +22,7 @@ import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestJiraConfig.class)
-@TestPropertySource(locations="classpath:test.properties")
+@TestPropertySource(locations = "classpath:test.properties")
 @Ignore
 /**
  * Some test methods if you want to play with local Jira
@@ -49,7 +53,7 @@ public class JiraDaoTest {
 
     @Test
     public void closeJiraIssue() throws JiraException {
-        jiraDao.closeIssue(1001,2,"partyo1");
+        jiraDao.closeIssue(1001, 2, "partyo1");
     }
 
 
@@ -70,6 +74,27 @@ public class JiraDaoTest {
         JSON json = jiraDao.getJiraClient().getRestClient().delete("/rest/api/2/issue/CLAIM-1");
         System.out.printf(String.valueOf(json));
 
+    }
+
+    @Test
+    public void testIgnore() {
+        Claim claim = new Claim();
+        Event event = new Event();
+        EventSource eventSource = new EventSource();
+        eventSource.setParty("39d17eca-0239-4ed8-8e32-dc78cf589135");
+        event.setSource(eventSource);
+        PartyEvent partyEvent = new PartyEvent();
+        partyEvent.setClaimCreated(claim);
+
+        EventPayload eventPayload = new EventPayload();
+        eventPayload.setPartyEvent(partyEvent);
+        event.setPayload(eventPayload);
+
+        SourceEvent sourceEvent = new SourceEvent();
+        sourceEvent.setProcessingEvent(event);
+        StockEvent stockEvent = new StockEvent();
+        stockEvent.setSourceEvent(sourceEvent);
+        new PartyEventHandler().handle(stockEvent);
     }
 
 }
